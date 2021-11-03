1e98696177e36cd89eafe64e05df2962a6158720
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-03 21:30:46 +0000

    pair program with Khushbakht Zehra Naqvi

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
+const transpose = require('../../d2/maxtrix_transpositon');
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    for (let i = 0; i < horizontalJoin.length; i++) {
+        l = horizontalJoin[i];
+        if (l === word) 
+        return true;
     }
+
+    let verticalJoin = transpose(letters)
+    verticalJoin = verticalJoin.map(ls => ls.join(''))
+    for (let i = 0; i < verticalJoin.length; i++) {
+        l = verticalJoin[i];
+        if (l.includes(word)) 
+        return true;
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch 
